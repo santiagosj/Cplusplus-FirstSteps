3f8bd59bc053abcf5f940bf73c41bc081af6f7d8
Santiago Spinetto Jung <sanjs965@gmail.com>
2019-02-15 09:38:44 -0300

    estructuras_de_control

---
+++ Estructuras_de_control/caraOcruz.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+
+int main(){
+
+
+  srand (time(NULL));
+
+  int coin = rand() % 2;
+
+  if(coin == 0){
+
+    std::cout << "cara" << '\n';
+  }else{
+    std::cout << "cruz" << '\n';
+  }
+}
