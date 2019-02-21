086ce8f2273f63c8de35a045aca387450abaab01
Santiago Spinetto Jung <sanjs965@gmail.com>
2019-02-21 09:08:15 -0300

    asignacion-dinamica

---
+++ Algoritmos_Ordenamiento/bubbleSort.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+
+template <typename RandomAccessIterator>
+
+void bubbleSort(RandomAccessIterator begin,RandomAccessIterator end){
+
+  bool swapped = true;
+
+  while (begin != end-- && swapped) {
+
+    swapped = false;
+
+    for (auto i = begin; i != end; i++) {
+
+      if (*(i + 1) < *i) {
+        std::iter_swap(i, i + 1);
+        swapped = true;
+      }
+
+    }
+
+  }
+
+}
+
+int main(){
+  int a[] = {100, 2, 56, 200, -52, 3, 99, 33, 177, -199};
+
+  bubbleSort(std::begin(a), std::end(a));
+  copy(std::begin(a), std::end(a),std::ostream_iterator<int>(std::cout, " "));
+  std::cout << '\n';
+}
--- first_steps/hello.cpp
+++ first_steps/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 
-  std::cout << "hola mundo! con c++" << '\n';
+  std::cout << "hola mundo! con c++\n";
 
   return 0;
 }
