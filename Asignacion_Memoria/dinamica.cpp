05284673aa4e25cb079d0c42bd3e9ea91425ef35
Santiago Spinetto Jung <sanjs965@gmail.com>
2019-02-21 09:21:40 -0300

    asignacion_dinamica

---
+++ Asignacion_Memoria/dinamica.cpp
@@ -0,0 +1,73 @@
+/*
+Asignacion dinamica de memoria
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <stdlib.h> //invoca new y delete, para asignar y liberar memoria;
+
+using namespace std;
+
+//Prototipos de funcion
+
+void pedirNotas();
+void mostrarNotas();
+void promedio();
+
+//declaracion de puntero y variables globales
+
+int numCalif, *calificacion;
+
+double promedioNotas;
+
+
+int main()
+{
+	pedirNotas();
+	mostrarNotas();
+	promedio();
+
+	delete[] calificacion; //liberando espacio
+
+	system("PAUSE");
+
+	return 0;
+}
+
+//definicion de funciones
+
+void pedirNotas()
+{
+	cout << "Ingresa el numero de calificaciones: \n";
+	cin >> numCalif;
+
+	calificacion = new int[numCalif]; //reserva de memoria, creacion del arreglo dinamico, se le asigna al puntero la primer posicion del arreglo dinamico.
+
+	for (int i = 0; i < numCalif; i++)
+	{
+		cout << "Ingresa la calificacion: ";
+		cin >> calificacion[i];
+	}
+}
+
+void mostrarNotas()
+{
+
+	cout << "\n\nMostrando notas: \n";
+
+	for (int i = 0; i < numCalif; i++)
+	{
+		cout << calificacion[i] << endl;
+	}
+
+}
+
+void promedio()
+{
+	for (int i = 0; i < numCalif; i++)
+	{
+		promedioNotas += calificacion[i];
+	}
+
+	cout << "El promedio es de: " << promedioNotas / numCalif << endl;
+}
