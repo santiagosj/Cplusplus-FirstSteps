81d2b99b0e3de0d80ed0aa8f64b87eff775b6643
Santiago Spinetto Jung <sanjs965@gmail.com>
2019-02-22 05:45:01 -0300

    int-value

--- first_steps/hello.cpp
+++ first_steps/hello.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+int x = 5;
+
+int y;
+
 int main(){
 
-  std::cout << "hola mundo! con c++\n";
+  std::cout << sizeof(int) << endl;
 
+  std::cout << "Hola mundo!" << '\n';
+
+  y = sizeof(x) / sizeof(int);
+
+  std::cout << y << '\n';
+  
   return 0;
 }
